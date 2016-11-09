093c167bf2e78e26c7cbef401f666b89dedd7f17
VictorKozachek <victor.kozachek@yandex.ru>
2016-11-09 11:18:02 +0300

    Person inheritance

--- src/main/java/ru/schoolarlife/logic/bo/person/Student.java
+++ src/main/java/ru/schoolarlife/logic/bo/person/Student.java
@@ -1,25 +1,17 @@
 package ru.schoolarlife.logic.bo.person;
 
-import ru.schoolarlife.logic.bo.lifecycle.Absences;
-import ru.schoolarlife.logic.bo.lifecycle.Assessment;
 import ru.schoolarlife.logic.bo.lifecycle.SchoolClass;
-import ru.schoolarlife.logic.bo.location.Address;
-import ru.schoolarlife.logic.helpers.Gender;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.util.Date;
 import java.util.Set;
 
 /**
  * Created by victor on 08.11.16.
  */
+@SuppressWarnings("PMD")
 @Entity
 @Table(name = "student")
 public class Student extends Person {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
 
     @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="class_id")
@@ -29,95 +21,4 @@ public class Student extends Person {
     @JoinTable(name = "parent_student", joinColumns = @JoinColumn(name = "parent_id"), inverseJoinColumns = @JoinColumn(name = "student_id"))
     private Set<Parent> parents;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    @NotNull
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    @NotNull
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @OneToOne(fetch=FetchType.LAZY)
-    @JoinColumn(name="address_id")
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public SchoolClass getSchoolClass() {
-        return schoolClass;
-    }
-
-    public void setSchoolClass(SchoolClass schoolClass) {
-        this.schoolClass = schoolClass;
-    }
-
-    public Set<Parent> getParents() {
-        return parents;
-    }
-
-    public void setParents(Set<Parent> parents) {
-        this.parents = parents;
-    }
 }
--- src/main/java/ru/schoolarlife/logic/bo/person/Teacher.java
+++ src/main/java/ru/schoolarlife/logic/bo/person/Teacher.java
@@ -1,23 +1,17 @@
 package ru.schoolarlife.logic.bo.person;
 
 import ru.schoolarlife.logic.bo.lifecycle.Subject;
-import ru.schoolarlife.logic.bo.location.Address;
-import ru.schoolarlife.logic.helpers.Gender;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.util.Date;
 import java.util.Set;
 
 /**
  * Created by victor on 08.11.16.
  */
+@SuppressWarnings("PMD")
 @Entity
 @Table(name = "teacher")
 public class Teacher extends Person {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
 
     @ManyToMany
     @JoinTable(
@@ -26,90 +20,4 @@ public class Teacher extends Person {
             inverseJoinColumns=@JoinColumn(name="subject_id", referencedColumnName="id"))
     private Set<Subject> subjects;
 
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Set<Subject> getSubjects() {
-        return subjects;
-    }
-
-    public void setSubjects(Set<Subject> subjects) {
-        this.subjects = subjects;
-    }
-
-    @NotNull
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    @NotNull
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    @NotNull
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    @NotNull
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @OneToOne(fetch=FetchType.LAZY)
-    @JoinColumn(name="address_id")
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
