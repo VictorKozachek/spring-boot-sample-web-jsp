d913b3a186b19ed5bcfc87f736c2ec1878780705
val.rudi <mail.valik@gmail.com>
2016-11-18 20:47:37 +0300

    Implemented JDBC authentication

--- src/main/java/ru/schoolarlife/config/WebSecurityConfig.java
+++ src/main/java/ru/schoolarlife/config/WebSecurityConfig.java
@@ -1,29 +1,45 @@
-package ru.schoolarlife.web.auth;
+package ru.schoolarlife.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+
 /**
- * Created by victor on 01.11.16.
+ * @author victor
  */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-   /* @Autowired
-    private UserDetailsService userDetailsService;*/
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public WebSecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery("select email, password, 'true' from user where email=?");
+        // TODO: Victor design this
+        //         .usersByUsernameQuery("select email, password, enabled from user where email=?");
+        //         .authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+
     }
 
     @Override
